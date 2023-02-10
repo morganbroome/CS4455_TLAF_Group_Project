da1600f396da3697f5e2db32611de1aa2d036547
gbroome3 <gbroome3@gatech.edu>
2023-02-10 18:45:59 -0500

    tidying up and demo build

--- Scrapy The Robot/Assets/Scripts/BulletController.cs
+++ Scrapy The Robot/Assets/Scripts/BulletController.cs
@@ -35,6 +35,7 @@ public class BulletController : MonoBehaviour
         } else
         {
             transform.position = parent.transform.position;
+            shootOn = false;
         }
     }
 }
